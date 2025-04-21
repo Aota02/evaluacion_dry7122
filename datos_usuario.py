24e059c80b995521b09e4bfb0d01edfc75d6d7b0
NetAcad DEVASC <changeme@example.com>
2025-04-21 02:08:02 +0000

    Agregado script datos_usuario.py

---
+++ datos_usuario.py
@@ -0,0 +1,11 @@
+# datos_usuario.py
+
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+codigo = input("Ingrese su Código-Sección: ")
+sede = input("Ingrese su sede: ")
+
+print("\n--- Datos Ingresados ---")
+print(f"Nombre completo: {nombre} {apellido}")
+print(f"Código-Sección: {codigo}")
+print(f"Sede: {sede}")
